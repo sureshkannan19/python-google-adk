9710aab0c6b4147ddc4bd331d25f5a54694ef285
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2026-05-18 18:22:15 +0530

    Google ADk Basic Agent

--- multi_agents/agent.py
+++ multi_agents/agent.py
@@ -1,3 +1,6 @@
+import datetime
+from zoneinfo import ZoneInfo
+
 from google.adk import Agent
 from dotenv import load_dotenv
 
@@ -5,7 +8,78 @@ load_dotenv()
 
 MODEL = "gemini-2.5-flash"
 
-root_agent  = Agent(name="travel_planner_agent",
-               model=MODEL,
-               description="An agent that helps users plan their travel itineraries",
-               instruction="You are a travel planner agent.")
+
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city (e.g., "New York", "London", "Tokyo").
+
+    Returns:
+        dict: A dictionary containing the weather information.
+              Includes a 'status' key ('success' or 'error').
+              If 'success', includes a 'report' key with weather details.
+              If 'error', includes an 'error_message' key.
+    """
+    print(f"--- Tool: get_weather called for city: {city} ---")  # Log tool execution
+    city_normalized = city.lower().replace(" ", "")  # Basic normalization
+
+    # Mock weather data
+    # api call
+    mock_weather_db = {
+        "newyork": {
+            "status": "success",
+            "report": "The weather in New York is sunny with a temperature of 25°C.",
+        },
+        "london": {
+            "status": "success",
+            "report": "It's cloudy in London with a temperature of 15°C.",
+        },
+        "tokyo": {
+            "status": "success",
+            "report": "Tokyo is experiencing light rain and a temperature of 18°C.",
+        },
+        "paris": {
+            "status": "success",
+            "report": "The weather in Paris is sunny with a temperature of 22°C.",
+        },
+    }
+
+    if city_normalized in mock_weather_db:
+        return mock_weather_db[city_normalized]
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Sorry, I don't have weather information for '{city}'.",
+        }
+
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (f"Sorry, I don't have timezone information for {city}."),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    return {"status": "success", "report": report}
+
+
+root_agent = Agent(name="travel_planner_agent",
+                   model=MODEL,  # to use local models, use LiteLlm(model_name)
+                   description="An agent that helps users plan their travel itineraries",
+                   instruction="You are a travel planner agent.",
+                   tools=[get_weather, get_current_time])
